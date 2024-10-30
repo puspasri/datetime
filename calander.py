24f0716afb26fdd44954b920d80e2793810ab128
puspasri <puspa09111984@gmail.com>
2024-10-30 17:52:06 +0530

    Add files via upload

---
+++ calander.py
@@ -0,0 +1,5 @@
+import calendar 
+
+month=12
+year=2100
+print(calendar.month(year,month))
---
+++ datetime.py
@@ -0,0 +1,6 @@
+from datetime import date,time,datetime
+
+#x = datetime.datetime.now()
+#print ("datetime", x)
+today=date.today()
+print (today)
